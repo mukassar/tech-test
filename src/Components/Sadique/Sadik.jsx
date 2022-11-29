55d29bc06362532e70527fb7b452ba58e2e0e3d6
sadikshaik@patent-art <sadikshaik@patent-art>
2022-11-29 14:55:35 +0530

    input box added

--- src/Components/Sadique/Sadik.jsx
+++ src/Components/Sadique/Sadik.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
  const Sadik = () => {
   return (
-    <div>Sadik- This is my component</div>
+    <div>Sadik- This is my component
+        <input type='text'></input>
+    </div>
   )
 }
 export default Sadik
