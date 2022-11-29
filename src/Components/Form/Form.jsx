e1a65e2d87d6020dbb33854c533e4d38bdc2854c
Shaikh Sadik <shaikhsadik9689@gmail.com>
2022-11-29 15:36:04 +0530

    form componat create

---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <div>
+            <label htmlFor="">Email Address</label>
+            <input type="text" />
+            <label htmlFor="">Password</label>
+            <input type="password" />
+        </div>
+    );
+};
+
+export default Form;
