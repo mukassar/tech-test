c25127a62e398477ee05f1f9a428172ea8fd6038
Shaikh Sadik <shaikhsadik9689@gmail.com>
2022-11-29 17:27:01 +0530

    file create

---
+++ src/Components/mukassar/Mukassar1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mukassar1 = () => {
+    return (
+        <div>
+            <h1>Latur</h1>
+        </div>
+    );
+};
+
+export default Mukassar1;<h1>Latur</h1>
